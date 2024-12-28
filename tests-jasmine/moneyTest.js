29c9223013458fe7e05b733347ba5041077f9c67
Lokendra Nath <lokendraverma225@gmail.com>
2024-12-28 14:44:20 +0530

    remove default tests and add money tests

---
+++ tests-jasmine/moneyTest.js
@@ -0,0 +1,11 @@
+import { formatPrice } from "../scripts/utils/money.js";
+
+describe("test suite : formateCurrency", () => {
+  it("convert cents into inr rupees formate", () => {
+    expect(formatPrice(2090)).toEqual("2,090");
+  });
+
+  it("work with 0", () => {
+    expect(formatPrice(0)).toEqual("0");
+  });
+});
