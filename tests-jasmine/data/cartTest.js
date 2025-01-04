9403547205da59c3f0a7d3ec85e8feda27ed24a1
Lokendra Nath <lokendraverma225@gmail.com>
2025-01-04 22:54:59 +0530

    testing cart

--- tests-jasmine/data/cartTest.js
+++ tests-jasmine/data/cartTest.js
@@ -1,11 +1,17 @@
-import { addToCart,cart } from "../../data/cart-class.js";
+import { addToCart, cart } from "../../data/cart.js";
 
-describe('test suite: addToCart()', () => {
-  it('adds an existing product to the cart', () => {
-    
+describe("test suite: addToCart()", () => {
+  beforeEach(() => {
+    spyOn(localStorage, "getItem").and.callFake(() => {
+      return JSON.stringify([]); // Mocked empty cart
+    });
   });
 
-  it('adds a new product to the cart', () => {
-    addToCart('83d4ca15-0f35-48f5-b7a3-1ea210004f2e')
+  it("should add an item to the cart", () => {
+    console.log(localStorage.getItem("cart")); // Logs the mocked value
+    
+    addToCart("83d4ca15-0f35-48f5-b7a3-1ea210004f2e"); // Call the function to test
+    expect(cart.length).toEqual(1); // Assertion for expected cart length
   });
 });
+
